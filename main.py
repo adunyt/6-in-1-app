f5eb78691e1a7b888b09765f3649cd2199e27789
adunyt <adunyt@gmail.com>
2022-10-31 12:17:10 +0300

    Changes: *Add python code for receive and send emails *Add login popup in mail app *Rewrite mail js/css/html

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import mailbox
+from mailbox import Mailbox
+from typing import Reversible
 import eel
-from imap_tools import MailBox, AND
+from imap_tools import MailBox, AND, A
+from email.mime.text import MIMEText
 import smtplib
+import ssl
+
+imap_servers = {
+    "Yandex": {"server": "imap.yandex.ru",
+               "port": 993},
+    "Google": {"server": "imap.google.ru",
+               "port": 993},
+}
+
+
+smpt_servers = {
+    "Yandex": {"server": "smtp.yandex.ru",
+               "port": 465},
+    "Google": {"server": "smpt.google.ru",
+               "port": 465},
+    "Localhost": {"server": "localhost",
+                  "port": 25},
+}
+
 
 @eel.expose
 def contact_saver(contact: str) -> str:
@@ -9,7 +30,7 @@ def contact_saver(contact: str) -> str:
     import os
     try:
         newContact = json.loads(contact)
-        file = os.open("contacts.json", os.O_RDWR|os.O_CREAT)
+        file = os.open("contacts.json", os.O_RDWR | os.O_CREAT)
         jsonFile: dict = json.load(file)
         contacts: list = jsonFile["contacts"]
         contacts.append(newContact)
@@ -19,11 +40,41 @@ def contact_saver(contact: str) -> str:
     finally:
         os.close(file)
 
+
+@eel.expose
+def getLetters(email, password, imap_name):
+    imap_server = imap_servers[imap_name]
+    with MailBox(imap_server["server"], imap_server["port"]).login(email, password) as mailbox:
+        messages = mailbox.fetch()
+        return messages
+
+
+@eel.expose
+def sendLetter(email: str, password: str, smtp_name: str, receivers: list, subject: str, message: str):
+    smtp_server = smpt_servers[smtp_name]
+
+    sender = email
+    msg = MIMEText(message)
+
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receivers
+
+    with smtplib.SMTP(smtp_server["server"], smtp_server["port"]) as server:
+        try:
+            server.login(email, password)
+            server.sendmail(sender, receivers, msg.as_string())
+            return {"status": "OK",
+                    "error_message": None}
+        except Exception as e:
+            return {"status": "Error",
+                    "error_message": e}
+
+
 try:
-    getLetters()
+    # getLetters("endercat1357@yandex.ru", "gymtnwklohdpqsvd")
     eel.init("web")
-    eel.start("index.html") 
+    eel.start("index.html")
 except Exception as e:
-    import ctypes  
+    import ctypes
     ctypes.windll.user32.MessageBoxW(0, str(e), "Ошибка!", 0)
-    
--- web/js/mail.js
+++ web/js/mail.js
@@ -1,15 +1,22 @@
-function addLetter(from, to, subject, message){
+let login;
+let password;
+
+window.onload = () => {
+  let popup = document.querySelector("#login-popup");
+  popup.style.display = "flex";
+};
+
+function toggleSpinner(){
+    let spinner = document.querySelector(".spinner");
+    spinner.classList.toggle("hidden");
+}
+
+function sendLetter(to, subject, message){
     console.log(`Отправляем письмо с заголовком ${subject} с сообщением "${message}" на почту ${to} от пользователя ${from}`);
-    
-    Email.send({
-        Host : "localhost",
-        Username : "username",
-        Password : "password",
-        To : to,
-        From : from,
-        Subject : subject,
-        Body : message
-    }).then(
-      message => alert(message)
-    );
+    ell.sendLetter(login, password, "Yandex", to, subject, message);   
+}
+
+function saveCreditals(email, password){
+  login = email;
+  password = password;
 }
